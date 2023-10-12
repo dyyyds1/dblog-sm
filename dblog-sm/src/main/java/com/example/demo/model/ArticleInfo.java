7a52d85738a184d411e3a2c41cf8c2528dce67a6
du-yang-1024 <1825391430@qq.com>
2023-10-12 14:37:48 +0800

    10.12

--- dblog-sm/src/main/java/com/example/demo/model/ArticleInfo.java
+++ dblog-sm/src/main/java/com/example/demo/model/ArticleInfo.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,6 +11,7 @@ public class ArticleInfo implements Serializable {
     private int id;
     private String title;
     private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "GMT+8")
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
     private int uid;
